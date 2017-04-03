e7a4f7678c8f699d1a6a9eb6c7ee8c1d9cd8d0eb
EVE DENISON <evedenison@gmail.com>
2017-04-03 11:22:31 -0700

    scaffolding and github cleint id

---
+++ Gogo-Github/Gogo-Github/GitHub.swift
@@ -0,0 +1,19 @@
+//
+//  GitHub.swift
+//  Gogo-Github
+//
+//  Created by Eve Denison on 4/3/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+import UIKit
+
+class GitHub {
+
+    let gitHubClientID = "46fedd5f05e0fa84bb6f"
+    let gitHubClientSecret = "46c6d589b2f108bc19651847027758284ef052f2"
+    
+    static let shared = GitHub()
+    
+    
+}
